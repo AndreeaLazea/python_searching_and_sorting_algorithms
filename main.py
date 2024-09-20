9d4b70e2c71cba30219d8c2092ecd30c66826c7d
Lazea Andreea <andreea.lazea@stud.ubbcluj.ro>
2024-09-20 16:08:24 +0300

    binary search added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def sequential_search(arr: list, target: float):
     is the one we search for
     :param arr: the list in which we search for the number
     :param target: the number we search for
-    :return: the position of the number in the array, if found,a nd -1 if not found
+    :return: the position of the number in the array, if found,and -1 if not found
     """
     size = len(arr)
     for i in range(size):
@@ -15,14 +15,49 @@ def sequential_search(arr: list, target: float):
             return i
     return -1
 
+
+def binary_search(arr: list, target: int):
+    """
+    algorithm for binary search.
+    Binary search can only be used on a sorted array.
+    It repeatedly divides the search interval in half.
+    The average time complexity is O(log n), the worst one is O(log n) and the best one is O(1).
+    It can only be O(1) if the number searched for is in the middle of the array.
+    :param arr: the list in which we search for the number
+    :param target: the number we search for
+    :return: the position of the number in the array if found, and -1 if not found
+    """
+    low = 0
+    high = len(arr)
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
 def main():
     arr = [10, -4, 5, 6, 23, 400]
     target = 23
-    returned_value = sequential_search(arr, target)
-    if returned_value == -1:
+
+    print("Sequential search: ")
+    sequential_returned_value = sequential_search(arr, target)
+    if sequential_returned_value == -1:
         print("We couldn't find " + str(target) + "in the array")
     else:
-        print(str(target) + " can be found at the " + str(returned_value) + " position in the array.")
+        print(str(target) + " can be found at the " + str(sequential_returned_value) + " position in the array.")
+
+    print("Binary Search: ")
+    binary_returned_value = binary_search(arr, target)
+    if binary_returned_value == -1:
+        print("We couldn't find " + str(target) + "in the array")
+    else:
+        print(str(target) + " can be found at the " + str(binary_returned_value) + " position in the array.")
+
 
 if __name__ == "__main__":
     main()
